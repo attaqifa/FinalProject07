726ccc53a05a4253f9c1bb3775c166590a6b987a
Fadhil <attaqifa@gmail.com>
2025-10-05 21:52:59 +0700

    update 1

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello GitHub Collaboration!");
+        System.out.println("ini adalah update ke-1");
     }
 }
