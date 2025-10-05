5bc6a3da93b184cd78af2ecb0ddbd561aab28107
Fadhil <attaqifa@gmail.com>
2025-10-05 21:56:31 +0700

    update 2

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello GitHub Collaboration!");
         System.out.println("ini adalah update ke-1");
+        System.out.println("ini adalah update ke-2");
     }
 }
